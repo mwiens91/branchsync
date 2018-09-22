6ee6bdef6e7bda441a57414083721a5b7d466a51
Matt Wiens <mwiens91@gmail.com>
2018-09-21 22:30:49 -0700

    Add config file path runtime arg

--- branchsync.js
+++ branchsync.js
@@ -1,8 +1,28 @@
 #!/usr/bin/env node
 
+// Load path package
+let path = require('path')
+
 // Allow help and version runtime flags with Yargs
-require('yargs')
+let argv = require('yargs')
   .usage('$0')
   .alias('h', 'help')
   .help()
+  .option('config', {
+    alias: 'c',
+    describe: 'path to config file',
+    type: 'string',
+    nargs: 1,
+    demand: true,
+    default: path.resolve('./config.yaml') // is this a sane default?
+  })
   .argv
+
+// Load config file
+
+// Execute each branch sync
+for (let i in [1, 2, 3]) {
+  // Fetch source branch
+
+  // Push to destination branch
+}
